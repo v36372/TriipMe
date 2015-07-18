f7486b5b2da07dfe8185b1f9b1a852cbff103654
tqduy <tqduy94@gmail.com>
2015-07-18 16:40:37 +0700

    add Footer

--- TriipMe/www/js/app.js
+++ TriipMe/www/js/app.js
@@ -8,6 +8,7 @@ TriipMeApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+	
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -18,3 +19,6 @@ TriipMeApp.run(function($ionicPlatform) {
 });
 
 
+
+
+	
