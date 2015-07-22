c4f317ef0a6376553896de1c6e09bc6751e1dda9
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2015-07-23 04:12:44 +0700

    + add blog details

--- TriipMe/www/js/controller/blogdetailsController.js
+++ TriipMe/www/js/controller/blogdetailsController.js
@@ -5,32 +5,48 @@ TriipMeApp.controller('blogdetailsController',['$scope','$cordovaCamera','$state
     $scope.blogid = $stateParams.blogid;
     console.log($scope.blogid);
     $scope.blog = {};
-    if($scope.blogid.length > 0)
-    {
-        fb.child("database").child("blogs").child($scope.blogid).once("value", function(data) {
-            $scope.blog = data.val();
+    //if($scope.blogid.length > 0)
+    //{
+    //    fb.child("database").child("blogs").child($scope.blogid).once("value", function(data) {
+    //        $scope.blog = data.val();
+    //    });
+    //}
+
+    fb.child("database").child("blogs").child($scope.blogid).once("value", function(data) {
+        $scope.blog = data.val();
+        $scope.blog.events = [];
+        console.log($scope.blog);
+        data.child("events").forEach(function(eventsnapshot){
+            var event = {};
+            console.log(eventsnapshot.key());
+            event.time = $scope.utilDateFormat(eventsnapshot.key());
+            event.title = eventsnapshot.child("title").val();
+            event.description = eventsnapshot.child("description").val();
+            $scope.blog.events.push(event);
         });
-    }
 
-    $scope.blog.events = [];
-    $scope.blog.desc = "It was the best thing that ever happened."
-    $scope.blog.img = 'img/hong-kong.jpg';
-    $scope.blog.title = "Destined Hong Kong"
+        console.log($scope.blog);
+    });
 
-    var event = {};
-    event.imgs = [];
-    event.imgs.push('img/lankwaifong.jpg');
-    event.imgs.push('img/girl.jpg');
-    event.time = (new Date()).toDateString();
-    event.desc = "I went to Lan Kwai Fong and met my soul mate there. It's destiny.";
-
-    $scope.blog.events.push(event);
-
-    var event1 = {};
-    event1.imgs = [];
-    event1.imgs.push('img/hongkong2.jpg');
-    event1.time = (new Date()).toDateString();
-    event1.desc = "I enjoyed the rest of the trip with Hong Kong traditional festival.";
-
-    $scope.blog.events.push(event1);
+    //$scope.blog.events = [];
+    //$scope.blog.desc = "It was the best thing that ever happened."
+    //$scope.blog.img = 'img/hong-kong.jpg';
+    //$scope.blog.title = "Destined Hong Kong"
+    //
+    //var event = {};
+    //event.imgs = [];
+    //event.imgs.push('img/lankwaifong.jpg');
+    //event.imgs.push('img/girl.jpg');
+    //event.time = (new Date()).toDateString();
+    //event.desc = "I went to Lan Kwai Fong and met my soul mate there. It's destiny.";
+    //
+    //$scope.blog.events.push(event);
+    //
+    //var event1 = {};
+    //event1.imgs = [];
+    //event1.imgs.push('img/hongkong2.jpg');
+    //event1.time = (new Date()).toDateString();
+    //event1.desc = "I enjoyed the rest of the trip with Hong Kong traditional festival.";
+    //
+    //$scope.blog.events.push(event1);
 }]);
