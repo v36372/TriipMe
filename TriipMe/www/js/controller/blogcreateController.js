faeed4b6bb1850f3df66742a6af957200e3101da
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2015-07-23 01:36:07 +0700

    + add event feature

--- TriipMe/www/js/controller/blogcreateController.js
+++ TriipMe/www/js/controller/blogcreateController.js
@@ -28,36 +28,37 @@ TriipMeApp.controller('blogcreateController',['$scope','$cordovaCamera','$state'
         $scope.newblog.author = fb.getAuth().uid;
 
         ////This code should be functional when photo.date is ready
-        //$scope.event = {};
-        //$scope.photosDate=[];
-        //$scope.photos.forEach(function(photo){
-        //
-        //    //if this photo date is the new day, note that each event should be classified by date not exact time (Day 1, day 2, ...)
-        //    if($scope.photosDate.indexOf(photo.date)==-1){
-        //        $scope.photosDate.push(photo.date);
-        //        $scope.event.title= photo.date;
-        //        $scope.event.imageList.push(photo);
-        //        $scope.event.description="Having fun with Triip@ "+ photo.location;
-        //        $scope.newblog.events.push($scope.event);
-        //        $scope.event={};
-        //    }
-        //    else
-        //    {
-        //          for (var i=0; i<$scope.newblog.events.length; i++) {
-        //                if($scope.newblog.events[i].title==photo.date){
-        //                    $scope.newblog.events[i].imageList.push(photo);
-        //                    }
-        //                };
-        //    };
-        //
-        //});
-        //if($scope.newblog.events.length==0)
-        //{
-        //    $scope.event.title= (new Date()).getTime();
-        //    $scope.event.description="Having fun with Triip, sorry no image now";
-        //    $scope.newblog.events.push($scope.event);
-        //
-        //}
+        $scope.event = {};
+        $scope.photosDate=[];
+        $scope.event.imageList = [];
+        $scope.photos.forEach(function(photo){
+
+            //if this photo date is the new day, note that each event should be classified by date not exact time (Day 1, day 2, ...)
+            if($scope.photosDate.indexOf(photo.date)==-1){
+                $scope.photosDate.push(photo.date);
+                $scope.event.title= photo.date;
+                $scope.event.imageList.push(photo);
+                $scope.event.description="Having fun with Triip@ "+ photo.location;
+                $scope.newblog.events.push($scope.event);
+                $scope.event={};
+            }
+            else
+            {
+                  for (var i=0; i<$scope.newblog.events.length; i++) {
+                        if($scope.newblog.events[i].title==photo.date){
+                            $scope.newblog.events[i].imageList.push(photo);
+                            }
+                        };
+            };
+
+        });
+        if($scope.newblog.events.length==0)
+        {
+            $scope.event.title= (new Date()).getTime();
+            $scope.event.description="Having fun with Triip, sorry no image now";
+            $scope.newblog.events.push($scope.event);
+
+        }
 
         blogsRef.push($scope.newblog);
         Camera.cleanup(null,null);
@@ -65,36 +66,52 @@ TriipMeApp.controller('blogcreateController',['$scope','$cordovaCamera','$state'
     };
 
     $scope.choosePicture = function(){
-        //window.imagePicker.getPictures(
-        //    function(results) {
-        //        for (var i = 0; i < results.length; i++) {
-        //            // DO STUFF HERE
-        //            console.log('Image URI: ' + results[i]);
-        //            $scope.processImg(results[i])
-        //        }
-        //    }, function (error) {
-        //        console.log('Error: ' + error);
-        //    }, {
-        //        // max images to be selected, defaults to 15. If this is set to 1, upon
-        //        // selection of a single image, the plugin will return it.
-        //        maximumImagesCount: 10,
-        //
-        //        // max width and height to allow the images to be.  Will keep aspect
-        //        // ratio no matter what.  So if both are 800, the returned image
-        //        // will be at most 800 pixels wide and 800 pixels tall.  If the width is
-        //        // 800 and height 0 the image will be 800 pixels wide if the source
-        //        // is at least that wide.
-        //        width: 300,
-        //        height: 300,
-        //
-        //        // quality of resized image, defaults to 100
-        //        quality: 50
-        //    }
-        //);
+        window.imagePicker.getPictures(
+            function(results) {
+                for (var i = 0; i < results.length; i++) {
+                    // DO STUFF HERE
+                    console.log('Image URI: ' + results[i]);
+                    $scope.processImg(results[i])
+                }
+            }, function (error) {
+                console.log('Error: ' + error);
+            }, {
+                // max images to be selected, defaults to 15. If this is set to 1, upon
+                // selection of a single image, the plugin will return it.
+                maximumImagesCount: 10,
 
-        CordovaExif.readData($scope.photo, function(exifObject) {
-            console.log(exifObject);
-        });
+                // max width and height to allow the images to be.  Will keep aspect
+                // ratio no matter what.  So if both are 800, the returned image
+                // will be at most 800 pixels wide and 800 pixels tall.  If the width is
+                // 800 and height 0 the image will be 800 pixels wide if the source
+                // is at least that wide.
+                width: 300,
+                height: 300,
+
+                // quality of resized image, defaults to 100
+                quality: 50
+            }
+        );
+        //var x = document.getElementById("test");
+        //var newImg = new Image();
+        //newImg.src = "img/Bush-dog.jpg"
+        //console.log("hello");
+        //var x = "data:image/jpeg;base64," + newImg;
+        ////document.getElementById("test").onclick = function() {
+        ////    console.log(this);
+        ////    EXIF.getData(this, function() {
+        ////        alert(EXIF.pretty(this));
+        ////    });
+        ////};
+        //console.log(newImg);
+        //EXIF.getData(newImg, function() {
+        //    alert(EXIF.pretty(this));
+        //});
+        //console.log(x);
+        //EXIF.getData(x, function(exifObject) {
+        //    console.log(exifObject);
+        //});
+
     };
 
     $scope.takePicture = function(){
@@ -111,8 +128,9 @@ TriipMeApp.controller('blogcreateController',['$scope','$cordovaCamera','$state'
 
         };
 
-        $cordovaCamera.getPicture(options).then($scope.processImg, function(err) {
+        $cordovaCamera.getPicture(options).then(img, function(err) {
             console.log(err);
+            $scope.processImg(img);
         });
     };
 
@@ -120,32 +138,40 @@ TriipMeApp.controller('blogcreateController',['$scope','$cordovaCamera','$state'
         var imgURI = "data:image/jpeg;base64," + img;
         //$scope.newblog.img = results[i];
 
-        $scope.photos.push(imgURI);
-        CordovaExif.readData(img, function(exifObject) {
-            console.log(exifObject);
+        var photo = {};
+        photo.img = imgURI;
+
+        EXIF.getData(img, function(exifObject) {
+            if (EXIF.getTag(this, "DateTimeOriginal") == null)
+                photo.date = EXIF.getTag(this, "DateTimeOriginal");
+            else
+                photo.date = EXIF.getTag(this, "DateTime");
+
             // GET LCOCATION NAME FROM LATTITUDE AND LONGTUTUDE
+            if(EXIF.getTag(this, "GPSLatitude") !== null && EXIF.getTag(this, "GPSLongitude") !== null) {
+                var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + EXIF.getTag(this, "GPSLatitude") +"," +  EXIF.getTag(this, "GPSLongitude") + "&key=AIzaSyB6LdCgpr-vnhuf9aC6RfslLWFiq41Bb7k";
 
-            var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=23.714224,78.961452&key=AIzaSyB6LdCgpr-vnhuf9aC6RfslLWFiq41Bb7k";
-
-            $.getJSON(geocodingAPI, function (json) {
-                if (json.status == "OK") {
-                    //Check result 0
-                    var result = json.results[0];
-                    //look for locality tag and administrative_area_level_1
-                    var city = "";
-                    var state = "";
-                    for (var i = 0, len = result.address_components.length; i < len; i++) {
-                        var ac = result.address_components[i];
-                        if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
+                $.getJSON(geocodingAPI, function (json) {
+                    if (json.status == "OK") {
+                        //Check result 0
+                        var result = json.results[0];
+                        //look for locality tag and administrative_area_level_1
+                        var city = "";
+                        var state = "";
+                        for (var i = 0, len = result.address_components.length; i < len; i++) {
+                            var ac = result.address_components[i];
+                            if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
+                        }
+                        if (state != '') {
+                            console.log("Hello to you out there in " + city + ", " + state + "!");
+                            photo.location = city;
+                        }
                     }
-                    if (state != '') {
-                        console.log("Hello to you out there in " + city + ", " + state + "!");
-                    }
-                }
 
-            });
+                });
+            }
         });
-
+        $scope.photos.push(photo);
         $('#img-container').css('height','250px')
     };
 
