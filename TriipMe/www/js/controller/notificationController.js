e1cbb5e507e84ec681be3892b9f7a8d9cd788d4c
v36372 <nguyentrongtin2331@gmail.com>
2015-07-23 14:43:50 +0700

    + add TriipMe route

--- TriipMe/www/js/controller/notificationController.js
+++ TriipMe/www/js/controller/notificationController.js
@@ -1,17 +1,20 @@
 TriipMeApp.controller('notificationController',['$scope','$state',function($scope,$state){
     $scope.notiList = [];
 
-    var notoRef = fb.child("database").child("users").child(fb.getAuth().uid).child("noti");
+    var notiRef = fb.child("database").child("users").child(fb.getAuth().uid).child("noti");
 
     notiRef.orderByKey().on("child_added",function(snapshot){
         var noti = snapshot.val();
         noti.time = $scope.utilDateFormat(snapshot.val().time);
         $scope.notiList.push(noti);
+
     });
 
     $scope.gotoBlog = function(blogid){
         //BlogID = fb.child("database").child("users").child(fb.getAuth().uid).child("blogs")
-        $state.go("blogdetails",{blogid:blogid});
+        console.log(blogid);
+        if(blogid !== null)
+            $state.go("blogdetails",{blogid:blogid});
 
     };
 }]);
