59a4d0381a89a587df50245d8d878a30a429b9a4
Son Nguyen <hoangson.2411@gmail.com>
2015-07-24 09:28:12 +0700

    ok

--- TriipMe/www/js/controller/blogcreateController.js
+++ TriipMe/www/js/controller/blogcreateController.js
@@ -27,224 +27,56 @@ TriipMeApp.controller('blogcreateController',['$scope','$cordovaCamera','$state'
         $scope.newblog.time = (new Date()).getTime();
         $scope.newblog.author = fb.getAuth().uid;
 
-        ////This code should be functional when photo.date is ready
+        /* This code should be functional when photo.date is ready
         $scope.event = {};
-        //$scope.photosDate=[];
-        $scope.event.imageList = [];
-        //$scope.trips = [];
-
-        var tripRef = fb.child("database").child("trips").child(fb.getAuth().uid);
-        //tripRef.once(fb.getAuth().uid,function(snapshot){
-        //    console.log(snapshot.val());
-        //    $scope.trips.push(snapshot.val());
-        //});
-
-        $scope.photos.forEach(function(photo) {
+        $scope.photosDate=[];
+        $scope.photos.forEach(function(photo){
             //if this photo date is the new day, note that each event should be classified by date not exact time (Day 1, day 2, ...)
-            //if($scope.photosDate.indexOf(photo.date)==-1){
-            //    $scope.photosDate.push(photo.date);
-            //    $scope.event.title= photo.date;
-            //    $scope.event.description="Having fun with Triip@ "+ photo.location;
-            //
-            //    tripRef.orderByChild("time_from").endAt(photo.date).once("value", function(snapshot) {
-            //        var trip = snapshot.val();
-            //
-            //        // found suitable trip
-            //        if(trip.time_to > photo.date){
-            //            photo.tripid = snapshot.key();
-            //            snapshot.child(events).forEach(function loop(eventsnapshot){
-            //                // found suitable event
-            //                if(loop.stop) {
-            //                    return;
-            //                }
-            //                var event = eventsnapshot.val()
-            //                if(eventsnapshot.key() <= photo.date && event.time_end >= photo.date){
-            //                    $scope.event.title = event.name;
-            //                    $scope.event.description = event.location;
-            //                    $scope.event.imageList.push(photo);
-            //                    loop.stop = true;
-            //                }
-            //            });
-            //        }
-            //
-            //        $scope.newblog.events.push($scope.event);
-            //    });
-            //
-            //    $scope.event={};
-            //}
-            //else
-            //{
-            //      for (var i=0; i<$scope.newblog.events.length; i++) {
-            //            if($scope.newblog.events[i].title==photo.date){
-            //                $scope.newblog.events[i].imageList.push(photo);
-            //                }
-            //            };
-            //};
-            //    var newevent = {};
-            //    var found = false;
-            //    newevent.title= photo.date;
-            //    newevent.description="Having fun with Triip@ "+ photo.location;
-            //    newevent.imageList.push(photo);
-            //    tripRef.orderByChild("time_from").endAt(photo.date).limitToLast(1).once("value", function(snapshot) {
-            //        var trip = snapshot.val();
-            //
-            //        // found suitable trip
-            //        if(trip.time_to > photo.date){
-            //            photo.tripid = snapshot.key();
-            //            snapshot.child(events).forEach(function loop(eventsnapshot){
-            //                // found suitable event
-            //                if(loop.stop) {
-            //                    return;
-            //                }
-            //                var event = eventsnapshot.val();
-            //                if(eventsnapshot.key() <= photo.date && event.time_end >= photo.date){
-            //                    newevent.title = event.name;
-            //                    newevent.description = event.location;
-            //                    //$scope.event.imageList.push(photo);
-            //                    found = true;
-            //                    if($scope.newblog.events[eventsnapshot.key()] !== null)
-            //                        $scope.newblog.events[eventsnapshot.key()].imageList.push(photo);
-            //                    else{
-            //                        //newevent.imageList.push(photo);
-            //                        $scope.newblog.events[eventsnapshot.key()] = newevent;
-            //                    }
-            //                    loop.stop = true;
-            //                }
-            //            });
-            //        }
-            //
-            //        if(!found){
-            //            $scope.newblog.events[photo.date] = newevent;
-            //        }
-            //    });
-            });
+            if($scope.photosDate.indexOf(photo.date)==-1){
+                $scope.photosDate.push(photo.date);
+                $scope.event.title= photo.date;
+                $scope.event.imageList.push(photo);
+                $scope.event.description="Having fun with Triip@ "+ photo.location;
+                $scope.newblog.events.push($scope.event);
+                $scope.event={};
+            }
+            else
+            {
+                  for (var i=0; i<$scope.newblog.events.length; i++) {
+                        if($scope.newblog.events[i].title==photo.date){
+                            $scope.newblog.events[i].imageList.push(photo);
+                            }                 
+                        };
+            };
+        });
+        if($scope.newblog.events.length==0)
+        {
+            $scope.event.title= (new Date()).getTime();
+            $scope.event.description="Having fun with Triip, sorry no image now";
+            $scope.newblog.events.push($scope.event);
+        }
+        */
+        blogsRef.push($scope.newblog);   
 
-            //if($scope.newblog.events.length==0)
-            //{
-            //    $scope.event.title= (new Date()).getTime();
-            //    $scope.event.description="Having fun with Triip, sorry no image now";
-            //    $scope.newblog.events[$scope.event.title] = $scope.event;
-            //
-            //}
 
-        blogsRef.push($scope.newblog);
-        //Camera.cleanup(null,null);
-        $state.go('home');
+
+        Camera.cleanup(null,null);    
+        $state.go('home');    
     };
 
-    $scope.choosePicture = function(){
-        //var newImg = new Image();
-        //newImg.src = "img/joy.jpg";
-        //EXIF.getData(newImg, function() {
-        //    alert(EXIF.pretty(this));
-        //});
-        window.imagePicker.getPictures(
-            function(results) {
-                for (var i = 0; i < results.length; i++) {
-                    // DO STUFF HERE
-                    console.log('Image URI: ' + results[i]);
-                    var photo = {};
-                    photo.src = results[i];
-                    var image = new Image();
-                    image.src = photo.src;
-
-                    image.onload = function(){
-                        EXIF.getData(image, function() {
-                            alert(EXIF.pretty(this));
-                            if (EXIF.getTag(this, "DateTimeOriginal") !== null) {
-                                photo.date = (new Date(EXIF.getTag(this, "DateTimeOriginal"))).getTime();
-                                fb.child("test").push(photo);
-                            }
-                            else{
-                                photo.date = (new Date(EXIF.getTag(this, "DateTime"))).getTime();
-                                fb.child("test").push(photo);
-                            }
-
-                            // GET LCOCATION NAME FROM LATTITUDE AND LONGTUTUDE
-                            if(EXIF.getTag(this, "GPSLatitude") !== null && EXIF.getTag(this, "GPSLongitude") !== null) {
-                                var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + EXIF.getTag(this, "GPSLatitude") +"," +  EXIF.getTag(this, "GPSLongitude") + "&key=AIzaSyB6LdCgpr-vnhuf9aC6RfslLWFiq41Bb7k";
-
-                                $.getJSON(geocodingAPI, function (json) {
-                                    if (json.status == "OK") {
-                                        //Check result 0
-                                        var result = json.results[0];
-                                        //look for locality tag and administrative_area_level_1
-                                        var city = "";
-                                        var state = "";
-                                        for (var i = 0, len = result.address_components.length; i < len; i++) {
-                                            var ac = result.address_components[i];
-                                            if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
-                                        }
-                                        if (state != '') {
-                                            console.log("Hello to you out there in " + city + ", " + state + "!");
-                                            photo.location = city;
-                                        }
-                                    }
-
-                                });
-                            }
-                        });
-                    };
-
-                    $scope.photos.push(photo);
-                    //$scope.processImg($scope.photos[0]);
-                    $('#img-container').css('height','250px')
-                }
-            }, function (error) {
-                console.log('Error: ' + error);
-            }, {
-                // max images to be selected, defaults to 15. If this is set to 1, upon
-                // selection of a single image, the plugin will return it.
-                maximumImagesCount: 10,
-
-                // max width and height to allow the images to be.  Will keep aspect
-                // ratio no matter what.  So if both are 800, the returned image
-                // will be at most 800 pixels wide and 800 pixels tall.  If the width is
-                // 800 and height 0 the image will be 800 pixels wide if the source
-                // is at least that wide.
-                width: 300,
-                height: 300,
-
-                // quality of resized image, defaults to 100
-                quality: 50
-            }
-        );
-        //var x = document.getElementById("test");
-        //var newImg = new Image();
-        //newImg.src = "img/joy.jpg";
-        //newImg.onload = function(){
-        //    EXIF.getData(newImg, function() {
-        //        alert(EXIF.pretty(this));
-        //    });};
-        //$timeout(function(){
-        //    EXIF.getData(newImg, function() {
-        //        alert(EXIF.pretty(this));
-        //    });
-        //},5000);
-        //console.log("hello");
-        //var x = "data:image/jpeg;base64," + newImg;
-        //document.getElementById("test").onclick = function() {
-        //    console.log(this);
-        //    EXIF.getData(this, function() {
-        //        alert(EXIF.pretty(this));
-        //    });
-        //};
-        //console.log(newImg);
-        //EXIF.getData(newImg, function() {
-        //    alert(EXIF.pretty(this));
-        //});
-        //console.log(x);
-        //EXIF.getData(x, function(exifObject) {
-        //    console.log(exifObject);
-        //});
-
+    $scope.choosePicture = function(){        
+        $scope.imageHandle(Camera.PictureSourceType.PHOTOLIBRARY);        
     };
 
     $scope.takePicture = function(){
+        $scope.imageHandle(Camera.PictureSourceType.CAMERA);
+    };
+
+    $scope.imageHandle = function(Type) {
         var options = {
             quality : 75,
             destinationType : Camera.DestinationType.DATA_URL,
-            sourceType : Camera.PictureSourceType.CAMERA,
+            sourceType : Type,
             allowEdit : true,
             encodingType: Camera.EncodingType.JPEG,
             targetWidth: 300,
@@ -254,148 +86,15 @@ TriipMeApp.controller('blogcreateController',['$scope','$cordovaCamera','$state'
 
         };
 
-        //$cordovaCamera.getPicture(options).then(img, function(err) {
-        //    console.log(err);
-        //});
-
         $cordovaCamera.getPicture(options).then(function(imageData) {
             var imgURI = "data:image/jpeg;base64," + imageData;
-            //$scope.newblog.img = results[i];
-
-            var photo = {};
-            photo.src = imgURI;
-
-            var image = new Image();
-            image.src = photo.src;
-
-            image.onload = function(){
-                EXIF.getData(image, function() {
-                    //alert(EXIF.pretty(this));
-                    if (EXIF.getTag(this, "DateTimeOriginal") !== null) {
-                        photo.date = (new Date(EXIF.getTag(this, "DateTimeOriginal"))).getTime();
-                        fb.child("test").push(photo);
-                    }
-                    else{
-                        photo.date = (new Date(EXIF.getTag(this, "DateTime"))).getTime();
-                        fb.child("test").push(photo);
-                    }
-
-                    // GET LCOCATION NAME FROM LATTITUDE AND LONGTUTUDE
-                    if(EXIF.getTag(this, "GPSLatitude") !== null && EXIF.getTag(this, "GPSLongitude") !== null) {
-                        var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + EXIF.getTag(this, "GPSLatitude") +"," +  EXIF.getTag(this, "GPSLongitude") + "&key=AIzaSyB6LdCgpr-vnhuf9aC6RfslLWFiq41Bb7k";
-
-                        $.getJSON(geocodingAPI, function (json) {
-                            if (json.status == "OK") {
-                                //Check result 0
-                                var result = json.results[0];
-                                //look for locality tag and administrative_area_level_1
-                                var city = "";
-                                var state = "";
-                                for (var i = 0, len = result.address_components.length; i < len; i++) {
-                                    var ac = result.address_components[i];
-                                    if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
-                                }
-                                if (state != '') {
-                                    console.log("Hello to you out there in " + city + ", " + state + "!");
-                                    photo.location = city;
-                                }
-                            }
-
-                        });
-                    }
-                });
-            };
-            $scope.photos.push(photo);
-
-            //$scope.photos.unshift(photo);
-
-            //$scope.processImg($scope.photos[0]);
+            $scope.newblog.img = imageData;
+            $scope.photos.push(imgURI);
             $('#img-container').css('height','250px')
         }, function(err) {
-            console.log(err);
+           console.log(err);
         });
-    };
-
-    $scope.processImg = function(photo){
-
-
-        //var image = new Image();
-        //image.src = photo.src;
-        //
-        //image.onload = function(){
-        //    EXIF.getData(image, function() {
-        //        if (EXIF.getTag(this, "DateTimeOriginal") == null) {
-        //            photo.date = (new Date(EXIF.getTag(this, "DateTimeOriginal"))).getTime();
-        //            fb.child("test").push(photo);
-        //        }
-        //        else{
-        //            photo.date = (new Date(EXIF.getTag(this, "DateTime"))).getTime();
-        //            fb.child("test").push(photo);
-        //        }
-        //
-        //        // GET LCOCATION NAME FROM LATTITUDE AND LONGTUTUDE
-        //        if(EXIF.getTag(this, "GPSLatitude") !== null && EXIF.getTag(this, "GPSLongitude") !== null) {
-        //            var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + EXIF.getTag(this, "GPSLatitude") +"," +  EXIF.getTag(this, "GPSLongitude") + "&key=AIzaSyB6LdCgpr-vnhuf9aC6RfslLWFiq41Bb7k";
-        //
-        //            $.getJSON(geocodingAPI, function (json) {
-        //                if (json.status == "OK") {
-        //                    //Check result 0
-        //                    var result = json.results[0];
-        //                    //look for locality tag and administrative_area_level_1
-        //                    var city = "";
-        //                    var state = "";
-        //                    for (var i = 0, len = result.address_components.length; i < len; i++) {
-        //                        var ac = result.address_components[i];
-        //                        if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
-        //                    }
-        //                    if (state != '') {
-        //                        console.log("Hello to you out there in " + city + ", " + state + "!");
-        //                        photo.location = city;
-        //                    }
-        //                }
-        //
-        //            });
-        //        }
-        //    });
-        //};
-        //
-        ////EXIF.getData(image, function() {
-        ////    if (EXIF.getTag(this, "DateTimeOriginal") == null) {
-        ////        photo.date = (new Date(EXIF.getTag(this, "DateTimeOriginal"))).getTime();
-        ////        fb.child("test").push(photo);
-        ////    }
-        ////    else{
-        ////        photo.date = (new Date(EXIF.getTag(this, "DateTime"))).getTime();
-        ////        fb.child("test").push(photo);
-        ////    }
-        ////
-        ////    // GET LCOCATION NAME FROM LATTITUDE AND LONGTUTUDE
-        ////    if(EXIF.getTag(this, "GPSLatitude") !== null && EXIF.getTag(this, "GPSLongitude") !== null) {
-        ////        var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + EXIF.getTag(this, "GPSLatitude") +"," +  EXIF.getTag(this, "GPSLongitude") + "&key=AIzaSyB6LdCgpr-vnhuf9aC6RfslLWFiq41Bb7k";
-        ////
-        ////        $.getJSON(geocodingAPI, function (json) {
-        ////            if (json.status == "OK") {
-        ////                //Check result 0
-        ////                var result = json.results[0];
-        ////                //look for locality tag and administrative_area_level_1
-        ////                var city = "";
-        ////                var state = "";
-        ////                for (var i = 0, len = result.address_components.length; i < len; i++) {
-        ////                    var ac = result.address_components[i];
-        ////                    if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
-        ////                }
-        ////                if (state != '') {
-        ////                    console.log("Hello to you out there in " + city + ", " + state + "!");
-        ////                    photo.location = city;
-        ////                }
-        ////            }
-        ////
-        ////        });
-        ////    }
-        ////});
-        ////$scope.photos.push(photo);
-        $('#img-container').css('height','250px')
-    };
+    }
 
 }]);
 
